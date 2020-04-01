7edbf3c1491d043aa134079ea9ffaf000cd5aca0
Paolo <p.fiaschi@hotmail.it>
2020-04-01 18:10:14 +0200

    inizio hamburger menu

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 //
 $($(document).ready(function() {
 
+  var dropMenu = $(".mainMenuDx ul li a");
+  var menuHamburger = $ ("#hamburgerMenu");
+  var closeIcon = $ (".close");
+  var hamburgerIcon = $ (".iconHamburgerMenu");
+
+  var elementoMostrato;
+
+  dropMenu.click(
+    function() {
+
+      if (elementoMostrato != null) {
+
+        elementoMostrato.toggle();
+        elementoMostrato = null;
+      }
+      elementoMostrato = $(this).siblings("div");
+      $(this).siblings("div").toggle();
+    }
+  );
+
+  // $(".mainMenuDx ul li").mouseenter(
+  //   function(){
+  //     $(".mainMenuDx ul li").children("div").hide();
+  //     $(this).children("div").show();
+  //   }
+  // )
+  hamburgerIcon.click(
+    function(){
+      hamburgerIcon.addClass('iconHamburgerMenuHide');
+      $(".menuTop").addClass('menuTopHide');
+      menuHamburger.show();
+      $(".mainMenuSx").addClass('mainMenuSxHide');
+    }
+  )
 
 
 
@@ -18,8 +52,5 @@ $($(document).ready(function() {
 
 
 
-
-
-
-
-});)
+})
+);
